1f8cc3194d6b6ada2f9365466045d0138a5e2922
Marcelo Melo <mm.ifce@gmail.com>
2020-09-24 14:15:22 -0300

    inicial commit

---
+++ build/precache-manifest.89dacfe1ed38d8ee41e00d8679cb3881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f59aa97543230e6f9fbb2782852b621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8aa9d58f45a6ab1f163",
+    "url": "/static/css/2.78485b03.chunk.css"
+  },
+  {
+    "revision": "e8aa9d58f45a6ab1f163",
+    "url": "/static/js/2.a83acb02.chunk.js"
+  },
+  {
+    "revision": "33f41c14c3f3c2199f4554d419e815a5",
+    "url": "/static/js/2.a83acb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4192c3055e8291945c82",
+    "url": "/static/js/main.c013342b.chunk.js"
+  },
+  {
+    "revision": "523eb7c2eb2dd16d0b2f",
+    "url": "/static/js/runtime-main.e85a5132.js"
+  },
+  {
+    "revision": "0db7588da2c2d99503ab98351b53d46f",
+    "url": "/static/media/logo ifce_ok.0db7588d.png"
+  }
+]);
